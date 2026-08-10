d3511234f2717591f84b358a314d08203a26c63b
Danny Orton <dan.orton@cityblock.com>
2026-08-10 13:55:29 -0600

    complete 4.7

--- obj/Debug/net10.0/CsharpProjects.AssemblyInfo.cs
+++ obj/Debug/net10.0/CsharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458f5fe80c9de105b2fa4853cd09038bdbf09697")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ef854bde6d6008bb3a9956abc53dd3848b4ff5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
